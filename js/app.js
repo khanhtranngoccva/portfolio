098220e29c53d6c2484c614c940c00da62e41507
khanhtranngoccva <khanhtranngoccva@gmail.com>
2022-04-27 23:41:45 +0700

    Chatbox

--- js/app.js
+++ js/app.js
@@ -17,7 +17,40 @@
         setTimeout(() => element.classList.add(animClass), 1);
     }
     
-    document.querySelector(".mainIconContainer").addEventListener("click", function (e) {
+    document.querySelector(".mainIcon").addEventListener("click", function (e) {
         startClassAnimation(this, "nudge");
     });
+    
+    HTMLElement.prototype.delegate = function (eventName, childSelector, callback) {
+        this.addEventListener(eventName, function (e) {
+            const currentElement = e.target.closest(childSelector);
+            if (currentElement === null || !this.contains(currentElement)) return undefined;
+            callback.call(currentElement, e);
+        });
+    }
+    
+    document.body.delegate("click", ".fa-xmark", function() {
+        fadeOut(document.querySelector("#" + this.getAttribute("targetElement")), 0.25);
+    });
+    
+    document.querySelector("#connect").addEventListener("click", ()=>fadeIn(document.querySelector("#linkTreeModal"), 0.25));
+
+    function fadeOut(element, time, easing="linear") {
+        element.style.removeProperty("animation");
+        element.style.animation = `fadeOut ${time}s ${easing} forwards`;
+        function hide() {
+            element.style.display = "none";
+            element.removeEventListener("animationend", hide);
+        }
+        element.addEventListener("animationend", hide);
+    }
+    
+    function fadeIn(element, time, easing="linear") {
+        element.style.removeProperty("animation");
+        element.style.removeProperty("display");
+        setTimeout(()=>{
+            element.style.animation = `fadeIn ${time}s ${easing} forwards`;
+            console.log("oof");
+        }, 5);
+    }
 })();
---
+++ js/bonus.js
@@ -0,0 +1,38 @@
+(function () {
+    let index = 0;
+    let textEntries = ["hey, click me!",
+        "do it again!",
+        "~clears throat~",
+        "Hi! I'm Khanh!",
+        "I'm a software engineer from Vietnam.",
+        "Been in tech for half a year.",
+        "Building awesome websites and software with #100devs.",
+        "Nice to meet you, and let's go get together!",
+        "You can follow me on Twitter where I post lots of uplifting stuff.",
+        "I forgot! Big shoutout to Savio @saviomartin7 for this textbox...",
+        "...and Brian @BrianSchneeDev for the portfolio gradient!",
+        "Also to all my awesome friends...",
+        "Thank you so much for inspiring me to do what I do...",
+        "... and helped me keep going through the trough of sorrow!",
+        "Have a nice day, and wish you all the best!",
+        "Don't forget to take care of yourself too! 😇",
+        "~silence~",
+        "~more silence~",
+        "~even more silence~",
+        "it's an awesome day outside, so just start doing something amazing!",
+        "don't you have anything better to do?"
+    ];
+
+    const bubbleTextElement = document.querySelector(".mainTextBox");
+
+    function nextTextForBubble() {
+        textEntries[index] && (bubbleTextElement.innerText = textEntries[index]);
+        index++;
+    }
+
+    nextTextForBubble();
+
+    document.querySelector(".mainIconContainer").addEventListener("click", function (e) {
+        nextTextForBubble();
+    });
+}());
