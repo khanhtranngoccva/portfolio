382e85dd399c80335d692b0954f0054c5a0c2138
khanhtranngoccva <khanhtranngoccva@gmail.com>
2022-04-27 21:49:29 +0700

    Optimization

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     const TEXT_TO_COLOR = {
         HTML: "#ff8f8f",
@@ -11,14 +11,13 @@
         let elementColor = TEXT_TO_COLOR[element.textContent];
         elementColor !== undefined && (element.style.backgroundColor = elementColor);
     }
-    
+
     function startClassAnimation(element, animClass) {
         element.classList.remove(animClass);
-        setTimeout(()=>element.classList.add(animClass), 1);
+        setTimeout(() => element.classList.add(animClass), 1);
     }
     
-    
-    document.querySelector(".mainIcon").addEventListener("click", function(e) {
+    document.querySelector(".mainIconContainer").addEventListener("click", function (e) {
         startClassAnimation(this, "nudge");
     });
 })();
---
+++ thumbnail.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import os
+
+
+def process_file(image_name, basepath):
+    image = Image.open(os.path.join(basepath, image_name))
+    image.resize((960, 540)).save(os.path.join(basepath, os.path.splitext(image_name)[0]+".webp"), format="WEBP")
+    image.close()
+
+
+for subfolder in os.listdir(".\\img"):
+    if os.path.isdir(f'img\\{subfolder}'):
+        for file in os.listdir(f'img\\{subfolder}'):
+            ext = os.path.splitext(file)[1]
+            if ext == ".svg":
+                pass
+            else:
+                process_file(file, f'img\\{subfolder}')
